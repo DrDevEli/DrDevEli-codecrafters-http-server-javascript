00ae46d73bc3b82d8fdca554c98c39e4057cd260
DrDevEli <e.m.arnald@gmail.com>
2024-06-14 00:37:06 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,11 +5,17 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.on("close", () => {
-        socket.end();
-        server.close();
-   });
+   
+    socket.on("data", (data)=>{
+        const path = data.toString().split(" ")[1];
+        const responseStatus = path === '/' ? "200 OK" : "404 Not Found";
+        socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+    
+     })
  });
+
+
+
+
 
  server.listen(4221, "localhost");
