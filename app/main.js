13df40ae999640a75a7ba70794987711a5125d70
DrDevEli <e.m.arnald@gmail.com>
2024-06-14 14:29:15 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require('fs');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -13,15 +14,24 @@ const server = net.createServer((socket) => {
         const url = request.split(" ")[1];
         const headers = request.split('\r\n');
         
-        if(url == "/"){
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        }else if(url.includes("/echo/")){
-            const content = url.split('/echo/')[1];
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+        if(url === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            else if(url.startsWith("/files/")){
+            const directory = process.argv[3];
+            const filename = url.split("/files/")[1];
+            if(fs.existsSync(`${directory}/${filename}`)){
+                const content = fs.readFileSync(`${directory}/${filename}`).toString();
+                const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+                socket.write(res);
+              } else {
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+              }
         }else if(url == "/user-agent"){
             const userAgent = headers[2].split('User-Agent: ')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
 
+        }else if(url.includes("/echo/")){
+            const content = url.split('/echo/')[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
         }else{
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
